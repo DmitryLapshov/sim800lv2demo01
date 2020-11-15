d46148405ddd1919c0fda090824cda18e9e4904c
Dmitry Lapshov <lycosa32@gmail.com>
2020-11-15 21:19:06 +0200

    Updated

--- sim800lv2demo01.ino
+++ sim800lv2demo01.ino
@@ -1,11 +1,13 @@
 #include <SoftwareSerial.h>
-// #define _SS_MAX_RX_BUFF 128 // RX buffer size in the \hardware\arduino\avr\libraries\SoftwareSerial\src\SoftwareSerial.h
+// Important!!! #define _SS_MAX_RX_BUFF 128 // RX buffer size in the \hardware\arduino\avr\libraries\SoftwareSerial\src\SoftwareSerial.h
 
 SoftwareSerial gsm(2, 3); //SIM800L Tx & Rx is connected to Arduino #2 & #3
 
 String buff;
 String message;
-String mynumber = "%2b"; // html encoded '+'
+String response;
+String incoming;
+String mynumber = "%2b";
 String token = "1234567890";
 
 void setup()
@@ -29,7 +31,7 @@ void setup()
   gsm.println("AT+CNUM");
   sendGSMtoSerial();
   mynumber.concat(buff.substring(22, 34));
-  Serial.println(mynumber);
+  Serial.println(mynumber); // My phone number
 }
 
 void loop()
@@ -44,7 +46,7 @@ void sendGSMtoSerial() {
     Serial.println(buff);
   }
   else {
-    buff = "";
+    buff = String("");
   }
 }
 
@@ -79,65 +81,65 @@ void readFromGSM()
 
   if (buff.indexOf("+CMT:") != -1)
   {
-    if (buff.indexOf("Status") != -1)
-    {
+    incoming = buff.substring(9, 22);
+    Serial.println(incoming);
+    
+    message = buff.substring(51);
+    message.trim();
+    message.replace(" ", "+");
+    
+    gsm.println("AT+CREG?");
+    sendGSMtoSerial();
+    
+    if (buff.indexOf("+CREG: 0,1") != -1) { // if connected to the network
+      gsm.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+      sendGSMtoSerial();
+      gsm.println("AT+SAPBR=3,1,\"APN\",\"internet\"");
+      sendGSMtoSerial();
+      gsm.println("AT+SAPBR=1,1");
+      sendGSMtoSerial();
+      gsm.println("AT+HTTPINIT");
+      sendGSMtoSerial();
+      gsm.println("AT+HTTPPARA=\"CID\",1");
+      sendGSMtoSerial();
+      gsm.print("AT+HTTPPARA=\"URL\",\"web.hosting-test.net/api/?token=");
+      gsm.print(token);
+      gsm.print("&source=");
+      gsm.print(mynumber);
+      gsm.print("&type=");
+      gsm.print("0");
+      gsm.print("&value=");
+      gsm.print(message);
+      gsm.println("\"");
+      // "web.hosting-test.net/api/?token=1234567890&source=%2b38095XXXXXXX&type=0&value=Test+is+a+test+01"
+      sendGSMtoSerial();
+      gsm.println("AT+HTTPSSL=0");
+      sendGSMtoSerial();
+      
+      gsm.println("AT+HTTPACTION=0");
+      sendGSMtoSerial();
+      
+      if (buff.indexOf("ERROR") == -1) {
+        sendGSMtoSerial();
+        gsm.println("AT+HTTPREAD");
+        sendGSMtoSerial();
+        response = buff.substring(11);
+        response.trim();
+      }
+      
+      gsm.println("AT+HTTPTERM");
+      sendGSMtoSerial();
+      gsm.println("AT+SAPBR=0,1");
+      sendGSMtoSerial();
+
       gsm.print("AT+CMGS=\"");
-      gsm.print(buff.substring(9, 22));
+      gsm.print(incoming);
       gsm.println("\"");
       sendGSMtoSerial();
-      gsm.print("I'm online!");
+      gsm.print(response);
       gsm.write(26);
       sendGSMtoSerial();
       sendGSMtoSerial();
-    }
-    else
-    {
-      message = buff.substring(51);
-      message.trim();
-      message.replace(" ", "+");
-      
-      gsm.println("AT+CREG?");
-      sendGSMtoSerial();
-      
-      if (buff.indexOf("+CREG: 0,1") != -1) { // if connected to the network
-        gsm.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
-        sendGSMtoSerial();
-        gsm.println("AT+SAPBR=3,1,\"APN\",\"internet\"");
-        sendGSMtoSerial();
-        gsm.println("AT+SAPBR=1,1");
-        sendGSMtoSerial();
-        gsm.println("AT+HTTPINIT");
-        sendGSMtoSerial();
-        gsm.println("AT+HTTPPARA=\"CID\",1");
-        sendGSMtoSerial();
-        gsm.print("AT+HTTPPARA=\"URL\",\"web.hosting-test.net/api/?token=");
-        gsm.print(token);
-        gsm.print("&source=");
-        gsm.print(mynumber);
-        gsm.print("&type=");
-        gsm.print("0");
-        gsm.print("&value=");
-        gsm.print(message);
-        gsm.println("\"");
-        // "web.hosting-test.net/api/?token=1234567890&source=%2b38095XXXXXXX&type=0&value=Test+is+a+test+01"
-        sendGSMtoSerial();
-        gsm.println("AT+HTTPSSL=0");
-        sendGSMtoSerial();
-        
-        gsm.println("AT+HTTPACTION=0");
-        sendGSMtoSerial();
-        
-        if (buff.indexOf("ERROR") == -1) {
-          sendGSMtoSerial();
-          gsm.println("AT+HTTPREAD");
-          sendGSMtoSerial();
-        }
-        
-        gsm.println("AT+HTTPTERM");
-        sendGSMtoSerial();
-        gsm.println("AT+SAPBR=0,1");
-        sendGSMtoSerial();
-      }
     }
   }
 }
